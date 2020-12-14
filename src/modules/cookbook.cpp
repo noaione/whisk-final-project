321392e27575b8e47d2c4f6709480cf34cc71c50
Andreas <andreas.alexander@binus.ac.id>
2020-12-14 23:03:25 +0700

    Update Cookbook

--- src/modules/cookbook.cpp
+++ src/modules/cookbook.cpp
@@ -1,9 +1,53 @@
 // include any modules here
-
 #include "cookbook.h"
+#include <stdio.h>
+#include<stdlib.h>
 
 // Dibutuhkan di sini:
 /*
     A library of users’ saved recipes which could be manually inputted or saved from
     online sources.
 */
+
+struct Node{
+    Cookbook lists;
+    Node *next, *prev;
+} *head, *tail, *curr;
+
+Node *create_node(Cookbook *recipe){
+    Node *temp = (Node *)malloc(sizeof(Node));
+    temp->lists = recipe;
+    temp->next = temp->prev = NULL;
+}
+
+void add_recipe(){
+
+}
+
+
+void cook_book(){
+    bool flag = true;
+    int option;
+    while(flag){
+        while(option > 3 || option < 1){
+            puts("1. View Cookbook");
+            puts("2. Add Recipe");
+            puts("3. Exit");
+            printf(">> ");
+            scanf("%d", &option);
+        }
+        switch (option){
+        case 1:
+            /* code */
+            break;
+
+        case 2:
+            /* code */
+            break;
+
+        case 3:
+            flag = false;
+            break;
+        }
+    }
+}
--- src/modules/cookbook.h
+++ src/modules/cookbook.h
@@ -2,5 +2,9 @@
 #define MOD_COOKBOOK_H
 
 // Definisikan semua struct dan sebagainya di sini.
+struct Cookbook{
+    char title[255];
+    char step[255];
+};
 
 #endif /* MOD_COOKBOOK_H */
